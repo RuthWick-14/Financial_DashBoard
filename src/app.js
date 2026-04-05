8723b6a70ec5ec9a2fc213a01d357d7641f7acc8
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2026-04-05 18:18:12 +0530

    ix import paths for routes and error handler in app.js

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ app.use(express.urlencoded({
     limit: "16kb"
 }))
 
-app.use("/api/v1/auth", require("./routes/auth.routes.js"));
-app.use("/api/v1/users", require("./routes/user.routes.js"));
-app.use("/api/v1/transactions", require("./routes/transaction.routes.js"));
-app.use("/api/v1/dashboard", require("./routes/dashboard.routes.js"));
+app.use("/api/v1/auth", require("../routes/auth.routes.js"));
+app.use("/api/v1/users", require("../routes/user.routes.js"));
+app.use("/api/v1/transactions", require("../routes/transaction.routes.js"));
+app.use("/api/v1/dashboard", require("../routes/dashboard.routes.js"));
 
-const errorHandler = require("./middleware/errorHandler.middleware");
+const errorHandler = require("./middleware/errorHandler.middleware.js");
 app.use(errorHandler);
 
 module.exports = app;
